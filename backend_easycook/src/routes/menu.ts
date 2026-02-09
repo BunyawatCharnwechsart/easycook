067b58d53a29323cf112af2a8aef194c79d75ac4
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2026-02-09 13:15:19 +0700

    get idmenu

--- backend_easycook/src/routes/menu.ts
+++ backend_easycook/src/routes/menu.ts
@@ -19,5 +19,29 @@ menu.get('/', async (c) => {
     }
 })
 
+//menu + วิธีทำ + วัตถุดิบ
+menu.get('/:menuid', async (c) => {
+    try {
+        const menuid = Number(c.req.param('menuid'))
+        if (Number.isNaN(menuid)) {
+            return c.json ({ error: 'Invalid menu id' }, 400)
+        }
+        const db = getDb(c)
+        const r = await db
+        .prepare(`SELECT * FROM menu WHERE menuid = ?`)
+        .bind(menuid)
+        .all()
+        const menu = r.results?.[0]
+        if (!menu) {
+            return c.json({ error: 'Not found' }, 404)
+        }
+        return c.json(menu)
+    } catch (err: any) {
+        return c.json(
+            { error: err?.message || String(err) },
+            500
+        )
+    }
+})
 
 export default menu
