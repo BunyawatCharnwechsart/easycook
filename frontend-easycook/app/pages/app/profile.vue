d2dcdd22b381372d962f9c7c5bd4982be51c29ea
fahsai <phattharaphontangtaeng718@gmail.com>
2026-02-16 13:28:48 +0700

    fah

--- frontend-easycook/app/pages/app/profile.vue
+++ frontend-easycook/app/pages/app/profile.vue
@@ -2,7 +2,7 @@
     <div class="carousel w-full">
   <div id="slide1" class="carousel-item relative w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+      src=""
       class="w-full" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide4" class="btn btn-circle">❮</a>
