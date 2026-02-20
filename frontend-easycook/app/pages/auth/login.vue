6058322ba79ee36a4d2785c9903000856d721c75
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2026-02-20 11:21:28 +0700

    create testapi page

--- frontend-easycook/app/pages/auth/login.vue
+++ frontend-easycook/app/pages/auth/login.vue
@@ -1,23 +1,9 @@
 <script setup>
-const config = useRuntimeConfig()
 
-const users = await $fetch(`${config.public.apiBase}/users`)
 </script>
 
 <template>
-    <div class="bg-amber-400 p-4">
-        <h1 class="text-xl font-bold mb-2">Fah Nuxt + Hono This is Login page</h1>
-        <ul>
-        <li
-            v-for="u in users"
-            :key="u.uid"
-            class="bg-white mb-2 p-2 rounded"
-        >
-            <p>ชื่อ: {{ u.name }}</p>
-            <p>Email: {{ u.email }}</p>
-        </li>
-        </ul>
-        <nuxt-link to="/auth/register">register</nuxt-link>
-        
-    </div>    
+    <div>
+        login
+    </div>
 </template>
